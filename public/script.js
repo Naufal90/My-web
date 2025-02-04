485e9f169ab714f6933207b8c3e4b5f1da25513a
Naufal90 <45502745+Naufal90@users.noreply.github.com>
2025-02-04 17:38:11 +0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const supabaseUrl = 'https://iafrlxyoeostvhnoywnv.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhZnJseHlvZW9zdHZobm95d252Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg1MzMwNjAsImV4cCI6MjA1NDEwOTA2MH0.WEdZeif209ew2iEWsGs9Y10529hDFI9BVdFvz_7Yeno'; // Kunci disembunyikan demi keamanan
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-// Server data dengan IP dan port
+// Server data dengan IP dan por
 const serverData = [
     { ip: "kimnetwork.zapto.org", ports: [20607], type: "Java" },
     { ip: "kimnetwork.zapto.org", ports: [20702], type: "Bedrock" }
