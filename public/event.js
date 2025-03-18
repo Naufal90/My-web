d2737bd908f2e9709bc7b32d82ed384399ddd7cc
Naufal90 <45502745+Naufal90@users.noreply.github.com>
2025-03-19 05:21:04 +0700

    Update event.js

--- public/event.js
+++ public/event.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", async function () {
+document.addEventListener("DOMContentLoaded", async () => {
     console.log("[DEBUG] Halaman dimuat. Menggunakan Supabase dari script.js...");
 
     if (!window.supabase) {
